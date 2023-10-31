8ad3db85a2396089de93045c02bce832bf1f8301
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-10-31 12:57:19 +0700

    Add Karyawan Level Activity-MainActivity
    Menambahkan Folder dan activity untuk home

---
+++ app/src/main/java/com/example/wmhanaasri/karyawanActivity/KaryawanMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.wmhanaasri.karyawanActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wmhanaasri.R;
+
+public class KaryawanMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_karyawan_main);
+    }
+}
