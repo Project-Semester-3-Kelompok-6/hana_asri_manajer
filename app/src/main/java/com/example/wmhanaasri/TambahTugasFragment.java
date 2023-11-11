031a85d7028163e8f730e1d707630c00a9114d7f
lazqii <gilangrizqi2016@gmail.com>
2023-11-11 08:35:54 +0700

    revisi layout login

--- app/src/main/java/com/example/wmhanaasri/TambahTugasFragment.java
+++ app/src/main/java/com/example/wmhanaasri/TambahTugasFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +26,9 @@ public class TambahTugasFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Spinner spinner;
+    private Spinner spinner2;
+    private SpinnerAdapter adapter;
 
     public TambahTugasFragment() {
         // Required empty public constructor
@@ -59,6 +65,31 @@ public class TambahTugasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tambah_tugas, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_tambah_tugas, container, false);
+        spinner = view.findViewById(R.id.spinDivisi);
+        spinner2 = view.findViewById(R.id.spinKepada);
+
+        ArrayAdapter<String> adapterDivisi = new ArrayAdapter<String>(requireActivity(), android.R.layout.simple_spinner_item);
+        adapterDivisi.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<String> adapterKepada = new ArrayAdapter<String>(requireActivity(), android.R.layout.simple_spinner_item);
+        adapterKepada.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        adapterDivisi.add("Pilih Divisi");
+        adapterDivisi.add("Mobile App Developer");
+        adapterDivisi.add("Marketing");
+
+        adapterKepada.add("Pilih Tujuan");
+        adapterKepada.add("Gilang Rizqi");
+        adapterKepada.add("Gilang Arjuna");
+
+        spinner.setAdapter(adapterDivisi); // Mengatur adapter ke Spinner
+        spinner.setSelection(0); // Memilih item "Pilih Divisi" sebagai item default
+
+        spinner2.setAdapter(adapterKepada); // Mengatur adapter ke Spinner
+        spinner2.setSelection(0); // Memilih item "Pilih Tujuan" sebagai item default
+
+        return view;
     }
 }
